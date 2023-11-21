2ce893479d92bd021026e07975e64688267ada92
Matthew Smart <matthewsmart13@gmail.com>
2023-11-20 19:25:00 -0600

    Created basic message model

---
+++ models/message.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const MessageSchema = new Schema({
+	user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+	message: { type: String, required: true },
+	postDate: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model('Message', MessageSchema);
