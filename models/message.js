e31e743d3675459c50446731c50f136ee537f829
Matthew Smart <matthewsmart13@gmail.com>
2023-11-20 21:47:49 -0600

    Added title to message model

--- models/message.js
+++ models/message.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema;
 
 const MessageSchema = new Schema({
 	user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+	title: { type: String, required: true },
 	message: { type: String, required: true },
 	postDate: { type: Date, default: Date.now },
 });
