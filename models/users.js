a28209247b918cb4e320acb38c865236731e7846
Matthew Smart <matthewsmart13@gmail.com>
2023-11-25 15:18:49 -0600

    Added uniaue and minLength to username and password

--- models/users.js
+++ models/users.js
@@ -3,11 +3,11 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-    username: { type: String, required: true },
-    password: { type: String, required: true },
-    isMember: { type: Boolean, required: true },
-    isAdmin: { type: Boolean, required: true },
-    dateJoined: { type: Date, default: Date.now },
+	username: { type: String, required: true, unique: true },
+	password: { type: String, required: true, minlength: 8 },
+	isMember: { type: Boolean, required: true },
+	isAdmin: { type: Boolean, required: true },
+	dateJoined: { type: Date, default: Date.now },
 });
 
 module.exports = mongoose.model('User', UserSchema);
