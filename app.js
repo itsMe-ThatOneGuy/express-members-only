29c59b784d9e0106d9c52bd41a60039a018b163b
Matthew Smart <matthewsmart13@gmail.com>
2023-11-25 18:33:48 -0600

    Added passport / bcrypt / session code

--- app.js
+++ app.js
@@ -5,7 +5,12 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 require('dotenv').config();
 
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
 const indexRouter = require('./routes/index');
+const authRouter = require('./routes/auth');
 
 const app = express();
 
@@ -18,9 +23,46 @@ async function main() {
 	await mongoose.connect(mongoDB);
 }
 
+const User = require('./models/users');
+const bcrypt = require('bcryptjs');
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+app.use(session({ secret: 'cats', resave: false, saveUninitialized: true }));
+
+passport.use(
+	new LocalStrategy(async (username, password, done) => {
+		try {
+			const user = await User.findOne({ username: username });
+			const match = await bcrypt.compare(password, user.password);
+			if (!user) {
+				return done(null, false, { message: 'Incorrect username' });
+			}
+			if (!match) {
+				return done(null, false, { message: 'Incorrect password' });
+			}
+			return done(null, user);
+		} catch (err) {
+			return done(err);
+		}
+	}),
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.serializeUser((user, done) => {
+	done(null, user.id);
+});
+passport.deserializeUser(async (id, done) => {
+	try {
+		const user = await User.findById(id);
+		done(null, user);
+	} catch (err) {
+		done(err);
+	}
+});
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -29,6 +71,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/auth', authRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
