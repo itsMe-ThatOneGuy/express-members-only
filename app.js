7881293f1b1a2b2e6c8aa101758e2246c9720f96
Matthew Smart <matthewsmart13@gmail.com>
2023-11-27 01:13:03 -0600

    Fixed passport db query bug

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use(session({ secret: 'cats', resave: false, saveUninitialized: true }));
 passport.use(
 	new LocalStrategy(async (username, password, done) => {
 		try {
-			const user = await User.findOne({ username: username });
+			const user = await User.findOne({ username: username }).exec();
 			const match = await bcrypt.compare(password, user.password);
 			if (!user) {
 				return done(null, false, { message: 'Incorrect username' });
@@ -57,7 +57,7 @@ passport.serializeUser((user, done) => {
 });
 passport.deserializeUser(async (id, done) => {
 	try {
-		const user = await User.findById(id);
+		const user = await User.findById(id).exec();
 		done(null, user);
 	} catch (err) {
 		done(err);
