41219640d211a17895afb6d361fe3b7da7614fe3
Matthew Smart <matthewsmart13@gmail.com>
2023-11-28 12:32:31 -0600

    Added minlength to username in user modal

--- models/users.js
+++ models/users.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-	username: { type: String, required: true, unique: true },
+	username: { type: String, required: true, minlength: 1, unique: true },
 	password: { type: String, required: true, minlength: 8 },
 	isMember: { type: Boolean, required: true },
 	isAdmin: { type: Boolean, required: true },
