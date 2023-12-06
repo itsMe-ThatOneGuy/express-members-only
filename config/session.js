600134e570069c470d5b8427e10aa530df5734e3
Matthew Smart <matthewsmart13@gmail.com>
2023-12-06 14:32:26 -0600

    Updated session config to make more secure

--- config/session.js
+++ config/session.js
@@ -1,9 +1,26 @@
-const expressSession = require('express-session');
+const MongoStore = require('connect-mongo');
+const express = require('express');
+const app = express();
 
-const session = expressSession({
+const expiration = new Date(Date.now() + 60 * 60 * 1000);
+
+const session = {
 	secret: process.env.SESSION_SECRET,
+	name: 'membersOnlyId',
 	resave: false,
+	store: MongoStore.create({
+		mongoUrl: process.env.MONGODB_URI,
+	}),
 	saveUninitialized: true,
-});
+	cookie: {
+		httpOnly: true,
+		expires: expiration,
+		sameSite: true,
+	},
+};
+
+if (app.get('env') === 'production') {
+	session.cookie.secure = true;
+}
 
 module.exports = session;
