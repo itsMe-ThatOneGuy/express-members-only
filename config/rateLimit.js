d8d72e7c5fd48fafaf5451ff6fd1de56125fed04
Matthew Smart <matthewsmart13@gmail.com>
2023-12-08 13:44:44 -0600

    Changed how rate limit config is being exported

--- config/rateLimit.js
+++ config/rateLimit.js
@@ -1,24 +1,41 @@
 const mongoose = require('mongoose');
 const { RateLimiterMongo } = require('rate-limiter-flexible');
 
-const maxConsecutiveFailsByUsernameAndIP = 10;
-const maxWrongAttemptsByIPperDay = 100;
+module.exports = async function () {
+	let mongoConn, mongooseInstance;
 
-module.exports.maxConsecutiveFailsByUsernameAndIP = 10;
-module.exports.maxWrongAttemptsByIPperDay = 100;
+	try {
+		mongooseInstance = await mongoose.connect(
+			process.env.NODE_ENV !== 'production'
+				? process.env.MONGODB_URI_DEV
+				: process.env.MONGODB_URI,
+		);
+		mongoConn = mongooseInstance.connection;
+	} catch (err) {
+		console.log(err);
+	}
 
-module.exports.limiterSlowBruteByIP = new RateLimiterMongo({
-	storeClient: mongoose.connection,
-	keyPrefix: 'login_fail_ip_per_day',
-	points: maxWrongAttemptsByIPperDay,
-	duration: 60 * 60 * 24,
-	blockDuration: 60 * 60 * 24,
-});
+	const maxConsecutiveFailsByUsernameAndIP = 5;
+	const maxWrongAttemptsByIPperDay = 100;
 
-module.exports.limiterConsecutiveFailsByUsernameAndIP = new RateLimiterMongo({
-	storeClient: mongoose.connection,
-	keyPrefix: 'login_fail_consecutive_username_and_ip',
-	points: maxConsecutiveFailsByUsernameAndIP,
-	duration: 60 * 60 * 24 * 90,
-	blockDuration: 60 * 60,
-});
+	return {
+		maxConsecutiveFailsByUsernameAndIP: 5,
+		maxWrongAttemptsByIPperDay: 100,
+
+		limiterSlowBruteByIP: new RateLimiterMongo({
+			storeClient: mongoConn,
+			keyPrefix: 'login_fail_ip_per_day',
+			points: maxWrongAttemptsByIPperDay,
+			duration: 60 * 60 * 24,
+			blockDuration: 60 * 60 * 24,
+		}),
+
+		limiterConsecutiveFailsByUsernameAndIP: new RateLimiterMongo({
+			storeClient: mongoConn,
+			keyPrefix: 'login_fail_consecutive_username_and_ip',
+			points: maxConsecutiveFailsByUsernameAndIP,
+			duration: 60 * 60 * 24 * 90,
+			blockDuration: 60 * 60,
+		}),
+	};
+};
